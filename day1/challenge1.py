6d1375da62bfb3535e85106183340a2f66f24d1e
Davide Corradi <davide.corradi.dev@gmail.com>
2025-12-02 00:01:21 +0100

    Day 1

---
+++ day1/challenge1.py
@@ -0,0 +1,29 @@
+import argparse
+from pathlib import Path
+
+def main(rotations):
+    counter = 0
+    accumulator = 50
+    for (direction, amount) in rotations:
+        accumulator = (accumulator + direction * amount) % 100
+        if accumulator == 0:
+            counter += 1
+    return counter
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", type=Path)
+    args = parser.parse_args()
+
+    rotations = []
+    with open(args.file_path) as file:
+        counter = 0
+        accumulator = 50
+        for line in file.readlines():
+            direction = -1 if line[0] == "L" else 1
+            amount = int(line[1:])
+            rotations.append((direction, amount))
+
+    result = main(rotations)
+    print(f"Result: {result}")
+
---
+++ day1/challenge2.py
@@ -0,0 +1,32 @@
+import argparse
+from pathlib import Path
+
+def main(rotations):
+    counter = 0
+    accumulator = 50
+    for (direction, amount) in rotations:
+        if direction > 0:
+            amount_to_zero = 100 - accumulator
+        else:
+            amount_to_zero = accumulator or 100
+        accumulator = (accumulator + direction * amount) % 100
+        counter += (amount - amount_to_zero) // 100 + 1
+    return counter
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", type=Path)
+    args = parser.parse_args()
+
+    rotations = []
+    with open(args.file_path) as file:
+        counter = 0
+        accumulator = 50
+        for line in file.readlines():
+            direction = -1 if line[0] == "L" else 1
+            amount = int(line[1:])
+            rotations.append((direction, amount))
+
+    result = main(rotations)
+    print(f"Result: {result}")
+
